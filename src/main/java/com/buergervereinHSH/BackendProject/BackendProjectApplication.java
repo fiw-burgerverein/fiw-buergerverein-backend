44c6f8caf1fdf445dc66546178d2df04b1480321
Kübra Demir <kubrademir@Kubras-MacBook-Pro.local>
2019-11-08 14:02:18 +0100

    Unser Backend Projekt

---
+++ src/main/java/com/buergervereinHSH/BackendProject/BackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.buergervereinHSH.BackendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buergervereinHSH/BackendProject/BackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buergervereinHSH.BackendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
