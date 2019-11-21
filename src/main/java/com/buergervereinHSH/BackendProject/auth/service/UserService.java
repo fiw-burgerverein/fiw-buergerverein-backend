e14e849fd5ec950e95ded14e5b7592d9988f0a9b
Kseniia Tereshenkova <ksenia24t@gmail.com>
2019-11-21 14:28:22 +0100

    UserService-Interface hinzugefügt

---
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/service/UserService.java
@@ -0,0 +1,13 @@
+package com.buergervereinHSH.BackendProject.auth.service;
+
+import com.buergervereinHSH.BackendProject.auth.ApiResponse;
+import com.buergervereinHSH.BackendProject.auth.dataTransferObject.LoginDto;
+import com.buergervereinHSH.BackendProject.auth.dataTransferObject.SignUpDto;
+
+public interface UserService {
+
+    ApiResponse signUp(SignUpDto signUpDto);
+
+    ApiResponse login(LoginDto loginDto);
+
+}
