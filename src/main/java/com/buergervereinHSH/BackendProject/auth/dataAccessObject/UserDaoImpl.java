a389a9b94d467def9c992bce69e0d6c204e23d77
Kseniia Tereshenkova <ksenia24t@gmail.com>
2019-11-21 14:15:39 +0100

    UserDaoImpl-Klasse hinzugefügt

---
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/dataAccessObject/UserDaoImpl.java
@@ -0,0 +1,22 @@
+package com.buergervereinHSH.BackendProject.auth.dataAccessObject;
+
+import com.buergervereinHSH.BackendProject.auth.model.User;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class UserDaoImpl {
+
+    @Autowired
+    private EntityManager em;
+
+    public User save(User user) {
+        Session session = em.unwrap(Session.class);
+        session.persist(user);
+        return user;
+    }
+
+}
