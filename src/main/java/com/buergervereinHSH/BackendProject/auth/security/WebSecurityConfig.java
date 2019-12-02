fddd28b1079f76b35aaa88883f7314173a1ca022
Kübra Demir <kubrademir@Kubras-MBP.home>
2019-12-02 23:29:15 +0100

    gesamte security implementiert

--- src/main/java/com/buergervereinHSH/BackendProject/auth/security/WebSecurityConfig.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/security/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.buergervereinHSH.BackendProject.auth.security;
 
+import com.buergervereinHSH.BackendProject.auth.security.jwt.JwtAuthEntryPoint;
+import com.buergervereinHSH.BackendProject.auth.security.jwt.JwtAuthTokenFilter;
+import com.buergervereinHSH.BackendProject.auth.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +13,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private JwtAuthEntryPoint unauthorizedHandler;
 
     @Bean
-    public JwtAuthTokenFilter authenticationJwtTokenFilter() {
+    public Filter authenticationJwtTokenFilter() {
         return new JwtAuthTokenFilter();
     }
 
