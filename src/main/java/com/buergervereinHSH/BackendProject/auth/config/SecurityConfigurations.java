be5c8047299ff45c6a8572348da74258d0c6819e
milena <pustet@posteo.de>
2019-12-02 16:12:03 +0100

    Security Configurations class added

--- src/main/java/com/buergervereinHSH/BackendProject/auth/config/SecurityConfigurations.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/config/SecurityConfigurations.java
@@ -1,4 +1,18 @@
 package com.buergervereinHSH.BackendProject.auth.config;
 
-public class SecurityConfigurations {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/confirm").permitAll();
+    }
 }
