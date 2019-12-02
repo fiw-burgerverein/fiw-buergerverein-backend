58b62dd500799332d3f174ff3e63e3c717ebb5ba
milena <pustet@posteo.de>
2019-12-02 16:11:09 +0100

    EmailSenderService class  added

--- src/main/java/com/buergervereinHSH/BackendProject/auth/service/EmailSenderService.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/service/EmailSenderService.java
@@ -1,4 +1,24 @@
 package com.buergervereinHSH.BackendProject.auth.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service("emailSenderService")  //@Service = variant of @Component
 public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        javaMailSender.send(email);
+    }
 }
