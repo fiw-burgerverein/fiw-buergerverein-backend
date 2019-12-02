188e505a10f1d294a17341b81bd49e72928465e1
Kübra Demir <kubrademir@Kubras-MBP.home>
2019-12-02 23:38:15 +0100

    request message Klassen implementiert

---
+++ src/main/java/com/buergervereinHSH/BackendProject/message/request/SignUpForm.java
@@ -0,0 +1,57 @@
+package com.buergervereinHSH.BackendProject.message.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class SignUpForm {
+
+    @NotBlank
+    @Size(min=3, max=50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
