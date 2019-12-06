fde418fdf06a52b5955d646eba86f4659eac924d
Kseniia Tereshenkova <ksenia24t@gmail.com>
2019-12-06 21:43:16 +0100

    Methode findByResetToken hinzugefügt

--- src/main/java/com/buergervereinHSH/BackendProject/auth/dataAccessObject/UserDao.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/dataAccessObject/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserDao extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+    User findByResetToken(String resetToken);
 }
 
 
