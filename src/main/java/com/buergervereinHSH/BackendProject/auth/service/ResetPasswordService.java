3877e4403c4d85e992f17957ded0b93da2db574d
Kseniia Tereshenkova <ksenia24t@gmail.com>
2019-12-06 22:02:05 +0100

    ResetPasswordService hinzugefügt

---
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/service/ResetPasswordService.java
@@ -0,0 +1,8 @@
+package com.buergervereinHSH.BackendProject.auth.service;
+
+import com.buergervereinHSH.BackendProject.auth.ApiResponse;
+import com.buergervereinHSH.BackendProject.auth.dataTransferObject.ForgotPasswordDto;
+
+public interface ResetPasswordService {
+    ApiResponse sendResetToken (ForgotPasswordDto forgotpasswordDto);
+}
