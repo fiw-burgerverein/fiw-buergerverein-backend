d6615528cdf4f7cbfdd5bbb0cb6dd78655430959
milena <pustet@posteo.de>
2019-12-10 15:13:29 +0100

    signUp Methode an UserDao angepasst & setEnabled hinzugefügt; fehler in login Methode bei enabled==false hinzugefügt

--- src/main/java/com/buergervereinHSH/BackendProject/auth/service/UserServiceImpl.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/service/UserServiceImpl.java
@@ -3,14 +3,12 @@ package com.buergervereinHSH.BackendProject.auth.service;
 import com.buergervereinHSH.BackendProject.auth.ApiResponse;
 import com.buergervereinHSH.BackendProject.auth.dataAccessObject.UserDao;
 import com.buergervereinHSH.BackendProject.auth.dataAccessObject.UserDaoImpl;
-import com.buergervereinHSH.BackendProject.auth.dataAccessObject.VerificationTokenRepository;
+import com.buergervereinHSH.BackendProject.auth.dataAccessObject.VerificationTokenDao;
 import com.buergervereinHSH.BackendProject.auth.dataTransferObject.LoginDto;
 import com.buergervereinHSH.BackendProject.auth.dataTransferObject.SignUpDto;
 import com.buergervereinHSH.BackendProject.auth.exceptions.*;
 import com.buergervereinHSH.BackendProject.auth.model.User;
 import com.buergervereinHSH.BackendProject.auth.model.VerificationToken;
-import com.buergervereinHSH.BackendProject.auth.service.EmailService;
-import com.buergervereinHSH.BackendProject.auth.service.EmailServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,21 +24,19 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
-    @Autowired
-    private UserDaoImpl userDaoImpl;
+   /* @Autowired
+    private UserDaoImpl userDaoImpl;*/
     @Autowired
     private BCryptPasswordEncoder encoder;
-
-    @Autowired //neu
-    private VerificationTokenRepository tokenRepository;
-
+    @Autowired
+    private VerificationTokenDao tokenRepository;
     @Autowired
     private EmailServiceImpl emailImpl;
 
     @Override
-    public ApiResponse signUp(SignUpDto signUpDto) {  //fehlt methode zur bestätigung des Links
+    public ApiResponse signUp(SignUpDto signUpDto) {
         validateSignUp(signUpDto);
-        User user = new User();   //Objekterzeugung in Spring
+        User user = new User();
 
         if(!signUpDto.getEmail().equals(signUpDto.getEmailConfirm())) {
             throw new EmailMismatchException();
@@ -54,24 +50,22 @@ public class UserServiceImpl implements UserService {
 
         BeanUtils.copyProperties(signUpDto, user);
         user.setPassword(encoder.encode(user.getPassword()));
-        userDaoImpl.save(user);
+        user.setEnabled(false);
+        userDao.save(user);
 
         String token = UUID.randomUUID().toString();  //erstellen eines random Strings (als Token)
         createVerificationTokenForUser(user, token);  //erstellen eines VerificationTokens mit token als String
 
         //zum testen, noch ohne URL in Email;
-        emailImpl.sendSimpleMessage(user.getEmail(), "Confirmation Registration", "Durch Betätigen" +
-                "des Links: "+token);
-        return new ApiResponse(200, "Ein Besätigungslink wurde an die von Ihnen angebenen Email gesendet.", user); //eigentlich noch nicht
+        emailImpl.sendSimpleMessage(user.getEmail(), "Confirmation Registration", "Bitte bestätigen Sie Ihren Account " +
+                "durch Betätigen des Links: "+token);
+        return new ApiResponse(200, "Ein Bestätigungslink wurde an die von Ihnen angebenen Email gesendet.", user);
     }
 
     @Override
     public ApiResponse login(LoginDto loginDto) {
 
         User user = userDao.findByEmail(loginDto.getEmail());
-
-
-        //if user.enabled==true:
 
         if(loginDto.getEmail() == null || loginDto.getPassword() == null) {
             throw new RuntimeException("Bitte füllen Sie die Email- und Passwort-Felder aus.");
@@ -98,12 +92,15 @@ public class UserServiceImpl implements UserService {
         //return new ApiResponse(200, "Token was created", null);
     }
 
+    private void validateSignUp(SignUpDto signUpDto) {
+    }
+
     /*@Override
     public void setEmailParam(User user, VerificationToken token) {
 
     }*/
 
-   /* @Override   //alten überschreiben, fehlt updateMethode (unr reicht?)
+   /* @Override   //alten Token überschreiben
     public VerificationToken generateNewVerificationToken(String existingVerificationToken) {
         VerificationToken vToken = tokenRepository.findByToken(existingVerificationToken);
         vToken.updateToken(UUID.randomUUID()
@@ -112,6 +109,4 @@ public class UserServiceImpl implements UserService {
         return vToken;
     }*/
 
-    private void validateSignUp(SignUpDto signUpDto) {
-    }
 }
