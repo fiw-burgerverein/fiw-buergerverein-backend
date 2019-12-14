d42238e05ec08dbc094bd42e9845e8d99456c664
Kseniia Tereshenkova <ksenia24t@gmail.com>
2019-12-14 23:23:21 +0100

    Entity-Klasse für Aufwandsentschädigungen erstellt

--- src/main/java/com/buergervereinHSH/BackendProject/forms/model/Aufwand.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/model/Aufwand.java
@@ -5,7 +5,21 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Aufwand")
 public class Aufwand {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @OneToOne(targetEntity = Formular.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "aufwandId")
     private long aufwandId;
+    private String Zweck;
+    private float cost;
+
+    public long getAufwandId() { return aufwandId; }
+    public void setAufwandId(long aufwandId) { this.aufwandId = aufwandId; }
+
+    public String getZweck() { return Zweck; }
+    public void setZweck(String zweck) { Zweck = zweck; }
+
+    public float getCost() { return cost; }
+    public void setCost(float cost) { this.cost = cost; }
+
 }
