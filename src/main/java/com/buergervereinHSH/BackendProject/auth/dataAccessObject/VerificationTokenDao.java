d5095bdbf057bca9bd5f1ea48194fa13839bbf56
milena <pustet@posteo.de>
2019-12-16 17:38:35 +0100

    Unnötige Methoden gelöscht

--- src/main/java/com/buergervereinHSH/BackendProject/auth/dataAccessObject/VerificationTokenDao.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/dataAccessObject/VerificationTokenDao.java
@@ -2,38 +2,12 @@ package com.buergervereinHSH.BackendProject.auth.dataAccessObject;
 
 import com.buergervereinHSH.BackendProject.auth.model.User;
 import com.buergervereinHSH.BackendProject.auth.model.VerificationToken;
-/*import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;*/
 import org.springframework.data.repository.CrudRepository;
 
-/*import java.util.Date;
-import java.util.stream.Stream;*/
-
-public interface VerificationTokenDao extends CrudRepository<VerificationToken, Long>  {
+public interface VerificationTokenDao extends CrudRepository<VerificationToken, Long> {
 
     VerificationToken findByToken(String token);
     VerificationToken save(VerificationToken token);
     VerificationToken findByUser(User user);
 
-
-    //not necessary now:
-/*    Stream<VerificationToken> findAllByExpiryDateLessThan(Date now);
-
-    void deleteByExpiryDateLessThan(Date now);
-
-    @Modifying
-    @Query("delete from VerificationToken t where t.expiryDate <= ?1")
-    void deleteAllExpiredSince(Date now);*/
 }
-
-//StackAbuse:
-/*
-import org.springframework.data.repository.CrudRepository;
-import com.buergervereinHSH.BackendProject.auth.model.VerificationToken;
-
-public interface VerificationTokenRepository extends CrudRepository<VerificationToken, String> {
-
-    VerificationToken findByVerificationToken(String verificationToken);
-}
-*/
