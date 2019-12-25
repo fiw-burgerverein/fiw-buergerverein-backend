bb47b2cb5a4c409ad6db9ff6baf148b5ab65bc9b
milena <pustet@posteo.de>
2019-12-25 18:15:35 +0100

    Crud hinzugefügt, alles auskommentiert vorerst

--- src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/AufwandDao.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/AufwandDao.java
@@ -1,12 +1,15 @@
+/*
 package com.buergervereinHSH.BackendProject.forms.dataAccessObject;
 
 import com.buergervereinHSH.BackendProject.forms.model.Aufwand;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface AufwandDao {
+public interface AufwandDao extends CrudRepository {
     //wenn ich das zum List machen wollte??
     List<Aufwand> findByFormId(long formId);
     List<Aufwand> getAllAufwande(long formId);
     Aufwand save(Aufwand aufwand);
 }
+*/
