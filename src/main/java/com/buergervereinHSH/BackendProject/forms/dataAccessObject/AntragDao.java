f25800435b8d5e8e1e03065988abcf75d61c8280
milena <pustet@posteo.de>
2019-12-25 18:14:15 +0100

    Dao Klasse falls nur ein Dao für Formular und Antrag

---
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/AntragDao.java
@@ -0,0 +1,12 @@
+/*
+package com.buergervereinHSH.BackendProject.forms.dataAccessObject;
+
+import com.buergervereinHSH.BackendProject.forms.model.Antrag;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AntragDao extends CrudRepository<Antrag, Long>{
+
+    Antrag save(Antrag antrag);
+    Antrag findByFormId (long formId);
+}
+*/
