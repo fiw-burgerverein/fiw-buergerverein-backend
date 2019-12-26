4c86265a3d8ef87b0e1eec7b5aece2c65a49e7f0
milena <pustet@posteo.de>
2019-12-26 10:05:29 +0100

    Repository Annotation

--- src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/FormDao.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/FormDao.java
@@ -2,12 +2,13 @@ package com.buergervereinHSH.BackendProject.forms.dataAccessObject;
 
 import com.buergervereinHSH.BackendProject.forms.model.Formular;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FormDao extends CrudRepository<Formular, Long> {
 
     Formular findByFormId(long formId);
     Formular findByEmail(String email);
     Formular save(Formular form);
-
     Formular findByUserId(long userId);
 }
