1e4f3bd33d190256f13c6a479cb65774fd621d46
milena <pustet@posteo.de>
2020-01-09 17:11:39 +0100

    Tabelle GeschStelle wird bei Aufrufen des Programms  mit (Test)daten der Geschäftstellen gefüllt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into gesch_stelle
+values ('01', 'NeuHSHNord@email.de', '25','Neu-Hohenschönhausen Nord', 'Berlin', '12345', 'NHSH Str.', '+49 30 12387345');
+insert into gesch_stelle
+values ('02', 'NeuHSHSued@email.de', '34', 'Neu-Hohenschönhausen Süd', 'Berlin', '464562', 'Sued Str.', '+49 30 156787345');
+insert into gesch_stelle
+values ('03', 'WaFaMal@email.de', '58b', 'Wartenberg, Falkenberg und Malchow', 'Berlin', '12345', 'WaFaMal Straße', '0176 458787345');
