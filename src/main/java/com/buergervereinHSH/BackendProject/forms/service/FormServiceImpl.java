b7dfeafcfb7df54f795e9a6422eb3980d4a02834
milena <pustet@posteo.de>
2020-01-09 17:31:24 +0100

    In saveForm Methode wird Email an entspr Geschäftstelle geschickt; Implementation von changeState Methode

--- src/main/java/com/buergervereinHSH/BackendProject/forms/service/FormServiceImpl.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/service/FormServiceImpl.java
@@ -1,7 +1,11 @@
 package com.buergervereinHSH.BackendProject.forms.service;
 
+import com.buergervereinHSH.BackendProject.auth.dataAccessObject.GeschStellenDao;
 import com.buergervereinHSH.BackendProject.auth.dataAccessObject.UserDao;
+import com.buergervereinHSH.BackendProject.auth.dataTransferObject.request.GeschStellenDto;
+import com.buergervereinHSH.BackendProject.auth.model.GeschStelle;
 import com.buergervereinHSH.BackendProject.auth.model.User;
+import com.buergervereinHSH.BackendProject.auth.service.EmailServiceImpl;
 import com.buergervereinHSH.BackendProject.auth.web.ApiResponse;
 import com.buergervereinHSH.BackendProject.forms.dataAccessObject.AufwandDao;
 import com.buergervereinHSH.BackendProject.forms.dataAccessObject.FormDao;
@@ -32,12 +36,16 @@ public class FormServiceImpl implements FormService {
     private AufwandDao aufwandDao;
     @Autowired
     private UserDao userDao;
-
+    @Autowired
+    private GeschStellenDao geschStellenDao;
     @Autowired
     private FormMailServiceImpl formMailServiceImpl;
+    @Autowired
+    private EmailServiceImpl emailImpl;
 
     @Override
     public ApiResponse saveForm(long userId, FormDto formDto) {
+
         Formular formular = new Formular();
         User user = userDao.findByUserId(userId);
         formular.setUser(user);
@@ -72,7 +80,14 @@ public class FormServiceImpl implements FormService {
         formular.setSachkostenSum(sachkostenGesamt);
         formDao.save(formular);
 
-//        eigentlich noch nicht da der Antrag noch in PDF umgewandelt werden soll und abgeschickt :/
+        // Email an entsprechende GS:
+        String GSName = formular.getOrt();
+        GeschStelle geschStelle = geschStellenDao.findByName(GSName);
+        String emailGS = geschStelle.getEmail();
+
+        emailImpl.sendSimpleMessage(emailGS, "Neuer Antrag eingegangen",
+                "Ein neu eingegangener Antrag liegt für Sie zum Download bereit.");
+
         return new ApiResponse(200, "Sie haben erfolgreich Ihren Antrag abgesendet!", null);
     }
 
@@ -91,6 +106,19 @@ public class FormServiceImpl implements FormService {
             return new ApiResponse(404, "Email konnte nicht gesendet werden", null);
         }
 
+    }
+
+    @Override
+    public ApiResponse changeState(long formId, GeschStellenDto geschStelleDto) {
+
+        Formular formular = formDao.findByFormId(formId);
+
+        int state = geschStelleDto.getState();
+        if(state==1) {formular.setStatus(Status.GENEHMIGT);}
+        else if(state==2) {formular.setStatus(Status.ABGELEHNT);}
+        else {formular.setStatus(Status.IN_BEARBEITUNG);}
+
+        return new ApiResponse(200, "Der Status des Antrags wurde erfolgreich geändert", formular);
     }
 
 }
