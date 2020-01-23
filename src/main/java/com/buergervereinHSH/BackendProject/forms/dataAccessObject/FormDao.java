0f5c26c4b1f7c1aec6a824d69b4486bb79ddfa8f
milena <pustet@posteo.de>
2020-01-23 16:02:18 +0100

    Methode zur Rückgabe aller Formulare hinzugefügt

--- src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/FormDao.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/dataAccessObject/FormDao.java
@@ -1,6 +1,7 @@
 package com.buergervereinHSH.BackendProject.forms.dataAccessObject;
 
 import com.buergervereinHSH.BackendProject.auth.model.User;
+import com.buergervereinHSH.BackendProject.forms.dataTransferObject.request.AllFormsDto;
 import com.buergervereinHSH.BackendProject.forms.model.Formular;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
@@ -18,9 +19,21 @@ public interface FormDao extends CrudRepository<Formular, Long> {
 
    // Formular[] findByUser(User user);
 
-
-/*    @Query(value="SELECT form_id from formular")
+    @Query("select formId from Formular")
     List<Long> getAllIds();
 
-    Iterable<Formular> findAllByFormId(Iterable<Long> formId);*/
+    @Query("select f from Formular f")
+    List<Formular> getAllForms();
+
+/*
+    @Query("select f.formId, f.createdAt, f.projectName, f.status from Formular f")
+    List<AllFormsDto> getAllForms();
+*/
+
+ /*   List<AllFormsDto> getAllForms();*/
+
+
+   /* @Query("select formId, createdAt, projectName, status from Formular")
+    List<AllFormsDto> getAllForms();*/
+    //Iterable<AllFormsDto> getAllForms(Iterable<AllFormsDto> formId);
 }
