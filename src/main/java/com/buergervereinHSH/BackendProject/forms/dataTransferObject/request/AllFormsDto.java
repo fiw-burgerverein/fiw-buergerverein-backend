86a292935cf6d019a597205f643734f85bdb54c7
milena <pustet@posteo.de>
2020-01-23 16:04:01 +0100

    Dto Klasse mit relevanten Attributen für Übersicht aller Anträge erstellt

---
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/dataTransferObject/request/AllFormsDto.java
@@ -0,0 +1,18 @@
+package com.buergervereinHSH.BackendProject.forms.dataTransferObject.request;
+
+import com.buergervereinHSH.BackendProject.forms.model.Status;
+
+import java.time.LocalDateTime;
+
+public class AllFormsDto {
+
+    private long formId;
+    private LocalDateTime createdAt;
+    private String projectName;
+    private Status status;
+
+
+
+
+
+}
