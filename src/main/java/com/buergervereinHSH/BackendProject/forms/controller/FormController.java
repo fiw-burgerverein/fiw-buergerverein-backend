8e0616fe1489eb1f0b6f4342ff704e6670ff8d86
milena <pustet@posteo.de>
2020-02-04 19:49:01 +0100

    Rechtevergabe der API Methoden in forms

--- src/main/java/com/buergervereinHSH/BackendProject/forms/controller/FormController.java
+++ src/main/java/com/buergervereinHSH/BackendProject/forms/controller/FormController.java
@@ -7,6 +7,7 @@ import com.buergervereinHSH.BackendProject.auth.web.ApiResponse;
 import com.buergervereinHSH.BackendProject.forms.dataTransferObject.request.FormDto;
 import com.buergervereinHSH.BackendProject.forms.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -21,16 +22,19 @@ public class FormController {
     private FormService formService;
 
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PostMapping("/{userId}/formular")
     public ApiResponse saveForm(@PathVariable("userId") long userId, @Valid @RequestBody FormDto formDto) {
         return formService.saveForm(userId, formDto);
     }
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @RequestMapping("/{userId}/formular/bestaetigen")  //hier nur zu testzwecken; wird wenn dann später Teil der saveForm methode
     public ApiResponse sendPDFToUser(@PathVariable("userId") long userId) throws MessagingException {
         return formService.sendPDFtoUser(userId);
     }
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping("/{formId}")
     public ApiResponse getSingleForm(@PathVariable("formId") long formId) {
         return formService.getSingleForm(formId);
