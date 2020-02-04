a971f307b83e2058e84d44e5b139e9b28a88e1d3
milena <pustet@posteo.de>
2020-02-04 17:04:23 +0100

    ein wenig aufgeräumt

--- src/main/java/com/buergervereinHSH/BackendProject/auth/dataTransferObject/request/SignUpDto.java
+++ src/main/java/com/buergervereinHSH/BackendProject/auth/dataTransferObject/request/SignUpDto.java
@@ -5,6 +5,7 @@ import com.buergervereinHSH.BackendProject.auth.model.Role;
 import javax.validation.constraints.*;
 import java.util.Set;
 
+//setter raus?
 public class SignUpDto {
 
     @NotBlank @NotNull @NotEmpty(message = "Sie müssen dieses Feld ausfüllen.")
@@ -19,12 +20,8 @@ public class SignUpDto {
     private String passwordConfirm;*/
     private Set<String> role;
 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
    /* public String getEmailConfirm() { return emailConfirm; }
     public void setEmailConfirm(String emailConfirm) { this.emailConfirm = emailConfirm; }
@@ -32,22 +29,15 @@ public class SignUpDto {
     public String getPasswordConfirm() { return passwordConfirm; }
     public void setPasswordConfirm(String passwordConfirm) { this.passwordConfirm = passwordConfirm; }
     */
-   public Set<String> getRole() {
-       return role;
-   }
+    public Set<String> getRole() { return role; }
 
-    public void setRole(Set<String> role) {
-        this.role = role;
-    }
-
+    public void setRole(Set<String> role) { this.role = role; }
 
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) { this.password = password; }
 
-    /*public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }*/
 
 
     /*
@@ -56,35 +46,10 @@ public class SignUpDto {
     @Email
     private String email;
 
-    private Set<String> role;
-
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
 
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<String> getRole() {
-        return role;
-    }
-
-    public void setRole(Set<String> role) {
-        this.role = role;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
      */
 }
