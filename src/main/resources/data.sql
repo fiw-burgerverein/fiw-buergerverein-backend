ed9aa84bfd2c10474a54f0da746cd0420943c9a0
milena <pustet@posteo.de>
2020-02-13 16:13:38 +0100

    daten in db werden nicht gelöscht bei Starten des Programms; Mail-Daten müssen eingefügt werden

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-insert into gesch_stelle
+/*insert into gesch_stelle
 values ('01', 'NeuHSHNord@email.de', '25','Neu-Hohenschönhausen Nord', 'Berlin', '12345', 'NHSH Straße', '+49 30 12387345');
 insert into gesch_stelle
 values ('02', 'NeuHSHSued@email.de', '34', 'Neu-Hohenschönhausen Süd', 'Berlin', '46562', 'Sued Straße', '+49 30 156787345');
 insert into gesch_stelle
 values ('03', 'Wartenberg@email.de', '58b', 'Wartenberg, Falkenberg und Malchow', 'Berlin', '12345', 'WaFaMal Straße', '+49 176 458787345');
-
+*/
 insert into roles values ( 1, 'ROLE_USER' );
 insert into roles values ( 2, 'ROLE_GS' );
 insert into roles values ( 3, 'ROLE_ADMIN' );
